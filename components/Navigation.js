ad62dab3a68119a4114bd54eb7d6a50006017ac3
unknown <isratjahan.emu1998@gmail.com>
2023-03-20 22:55:03 +0600

    ..

--- components/Navigation.js
+++ components/Navigation.js
@@ -10,56 +10,34 @@ const Navigation = () => {
         </div>
         <ul className="items-stretch hidden space-x-3 lg:flex backdrop-blur-sm skillbg border border-gray-700 px-4 rounded-md">
           <li className="flex items-center">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="flex items-center px-4 hover:bg-gray-500/30 py-1 rounded"
-            >
+            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 hover:bg-gray-500/30 py-1 rounded">
               About
             </a>
           </li>
           <li className="flex items-center">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="flex items-center px-4 hover:bg-gray-500/30 py-1 rounded"
-            >
+            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 hover:bg-gray-500/30 py-1 rounded">
               Experience
             </a>
           </li>
           <li className="flex items-center">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="flex items-center px-4 hover:bg-gray-500/30 py-1 rounded"
-            >
+            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 hover:bg-gray-500/30 py-1 rounded">
               Project
             </a>
           </li>
           <li className="flex items-center">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="flex items-center px-4 hover:bg-gray-500/30 py-1 rounded"
-            >
+            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 hover:bg-gray-500/30 py-1 rounded">
               Education
             </a>
           </li>
           <li className="flex items-center">
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="flex items-center px-4 hover:bg-gray-500/30 py-1 rounded"
-            >
+            <a rel="noopener noreferrer" href="#" className="flex items-center px-4 hover:bg-gray-500/30 py-1 rounded">
               Contact
             </a>
           </li>
         </ul>
         <div className="flex items-center">
           <div className="items-center flex-shrink-0 hidden lg:flex ml-3">
-            <button className="px-4 py-2 rounded border-2 border-primary text-primary ">
-              Resume
-            </button>
+            <button className="px-4 py-2 rounded border-2 border-primary text-primary ">Resume</button>
           </div>
         </div>
       </div>
